dc149bccf80f9f225f9fc6db0036a027651e5968
a-colavito <adolfocolavito@hotmail.it>
2025-01-13 20:27:29 +0100

    simmetrizzato stili menu

--- Script/menuLogic.js
+++ Script/menuLogic.js
@@ -4,21 +4,39 @@ export const setupMenuLogic = () => {
     const toggleButton = document.getElementById('toggle-menu');
     const leftMenu = document.getElementById('left-menu');
     const campus = document.getElementById('bari-campus');
+    const taranto = document.getElementById('taranto');
+    const amministrazione = document.getElementById('amministrazione');
+    const mapMenu = document.getElementById('map-menu');
 
+    // Funzione per nascondere il mapMenu
+    const hideMapMenu = () => {
+        mapMenu.classList.add('hidden');
+    };
+
+    // Funzione per mostrare il mapMenu
+    const showMapMenu = () => {
+        mapMenu.classList.remove('hidden');
+    };
+
+    // Event listener per il campus
     campus.addEventListener('click', () => {
         map.setView([41.1088530, 16.8789227], 18);
+        hideMapMenu();
     });
 
-    const taranto = document.getElementById('taranto');
+    // Event listener per Taranto
     taranto.addEventListener('click', () => {
         map.setView([40.526069, 17.281923], 18);
+        hideMapMenu();
     });
 
-    const amministrazione = document.getElementById('amministrazione');
+    // Event listener per Amministrazione
     amministrazione.addEventListener('click', () => {
         map.setView([41.111413, 16.883224], 18);
+        hideMapMenu();
     });
 
+    // Toggle del menu laterale
     toggleButton.addEventListener('click', () => {
         leftMenu.classList.toggle('collapsed');
         const locationButtons = document.getElementById('location-buttons');
@@ -31,5 +49,10 @@ export const setupMenuLogic = () => {
             locationButtons.style.display = 'block';
             resourceList.style.display = 'block';
         }
+    });
+
+    // Riattiva il mapMenu se necessario, ad esempio con un evento sulla mappa
+    map.on('click', () => {
+        showMapMenu();
     });
 };
