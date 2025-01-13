5fa85d0972d5ca1bcf6a3bfe8e965b3114a2a817
a-colavito <adolfocolavito@hotmail.it>
2025-01-13 19:46:15 +0100

    aggiunta view mobile

--- Script/mobileView.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const mapMenu = document.getElementById("map-menu");
-    const menuHeader = mapMenu.querySelector(".menu-header");
-
-    menuHeader.addEventListener("click", function () {
-        mapMenu.classList.toggle("expanded");
-    });
-});
