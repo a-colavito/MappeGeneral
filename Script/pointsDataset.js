27642f639c94d5050463c96e2ce9b33b07065bd3
a-colavito <adolfocolavito@hotmail.it>
2025-01-13 16:00:36 +0100

    sisstemato dataset

--- Script/pointsDataset.js
+++ Script/pointsDataset.js
@@ -7,16 +7,16 @@ export const points = [
         [ 41.1088191, 16.8784419, "Biblioteca Polilibrary", "library", "Biblioteche", "Campus “Ernesto Quagliariello”,\n Via E. Orabona, 4 - 70125 Bari", "Terzo Piano", "Corpo a Z", "Posti a sedere: 128\nPostazioni Internet: 16" ],
         [ 41.109444, 16.878766, "Biblioteca Vitruvius", "library", "Biblioteche", "Campus “Ernesto Quagliariello”,\n Via E. Orabona, 4 - 70125 Bari", "Piano Terra", "Dipartimento di Architettura, Costruzione e Design", "Posti a sedere: 86 \n Postazioni Internet: 7" ],
         [ 41.109429, 16.878465, "Aula Magna Domus Sapientiae", "auditorium", "Aula Magna", "Campus “Ernesto Quagliariello”,\n Via E. Orabona, 4 - 70125 Bari", "Piano Terra", "Dipartimento di Architettura, Costruzione e Design" ],
-        [ 41.1079490020772, 16.879550680607615, "Student Center", "library", "Biblioteche", "Campus “Ernesto Quagliariello”,\n Via E. Orabona, 4 - 70125 Bari", "Primo e Secondo Piano", "Plesso Grandi Aule" , " 148 posti al primo piano \n 96 posti al secondo piano "],
-        [41.107867, 16.879157, "Presidio Primo Soccorso", "hospital", "Primo Soccorso","Campus “Ernesto Quagliariello”,\n Via E. Orabona, 4 - 70125 Bari", "Piano Terra", "Ingresso Pedonale Via Orabona" , "", "Lunedì-Venerdì: 8-20 \n Sabato: 8-13"],
-        [41.109126, 16.879670, "Consueling Psicologico", "consueling", "Consueling Psicologico","Campus “Ernesto Quagliariello”,\n Via E. Orabona, 4 - 70125 Bari" , "Piano Terra", "Plesso “Luigi Salvatore”", "", "Lunedì-Venerdì: 9.30-12.30" ],
-        [41.1080290, 16.8805320, "Atm Intesa San Paolo - Bari Campus", "atm", "ATM Intesa San Paolo  Bari Campus", "Campus “Ernesto Quagliariello”,\n Via E. Orabona, 4 - 70125 Bari", "Piano Terra" , "Ingresso Principale Via E. Orabona 4"]
+        [ 41.1079490020772, 16.879550680607615, "Student Center", "library", "Biblioteche", "Campus “Ernesto Quagliariello”,\n Via E. Orabona, 4 - 70125 Bari", "Primo e Secondo Piano", "Plesso Grandi Aule", " 148 posti al primo piano \n 96 posti al secondo piano " ],
+        [ 41.107867, 16.879157, "Presidio Primo Soccorso", "hospital", "Primo Soccorso", "Campus “Ernesto Quagliariello”,\n Via E. Orabona, 4 - 70125 Bari", "Piano Terra", "Ingresso Pedonale Via Orabona", "", "Lunedì-Venerdì: 8-20 \n Sabato: 8-13" ],
+        [ 41.109126, 16.879670, "Consueling Psicologico", "consueling", "Consueling Psicologico", "Campus “Ernesto Quagliariello”,\n Via E. Orabona, 4 - 70125 Bari", "Piano Terra", "Plesso “Luigi Salvatore”", "", "Lunedì-Venerdì: 9.30-12.30" ],
+        [ 41.1080290, 16.8805320, "Atm Intesa San Paolo - Bari Campus", "atm", "ATM Intesa San Paolo  Bari Campus", "Campus “Ernesto Quagliariello”,\n Via E. Orabona, 4 - 70125 Bari", "Piano Terra", " Gabbiotto Ingresso Principale Via E. Orabona 4" ]
     ],
     ...[
-        [ 40.526069, 17.281923, "Politecnico di Bari sede di Taranto", "auditorium", "Aula Magna" ,"Centro Interdipartimentale “Magna Grecia” \n Viale del Turismo, 8 - 74121 Taranto", "Piano Terra", "Ingresso Principale"  ],
+        [ 40.526069, 17.281923, "Politecnico di Bari sede di Taranto", "auditorium", "Aula Magna", "Centro Interdipartimentale “Magna Grecia” \n Viale del Turismo, 8 - 74121 Taranto", "Piano Terra", "Ingresso Principale" ],
         [ 40.526446, 17.281896, "Biblioteca Magna Grecia", "library", "Biblioteche", "Centro Interdipartimentale “Magna Grecia” \n Viale del Turismo, 8 - 74121 Taranto", "Piano Terra", "Vecchio Plesso Ex Facoltà", "Posti a sedere: 70 \n Postazioni Internet: 2" ],
     ],
     ...[
-        [ 41.111413, 16.883224, "Politecnico di Bari, Servizi Amministrativi", "desk", "Amministrazione", "Via Giovanni Amendola 126/b, 70126 Bari BA" , "Piano Terra", "Politecnico di Bari - Segreteria Uffici", "", "" ],
+        [ 41.111413, 16.883224, "Politecnico di Bari, Servizi Amministrativi", "desk", "Amministrazione", "Via Giovanni Amendola 126/b, 70126 Bari BA", "Piano Terra", "Politecnico di Bari - Segreteria Uffici", "", "" ],
     ],
 ];
