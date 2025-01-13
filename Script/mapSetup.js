7d7b87d261a38156c944d62d85d75c23a34be490
a-colavito <adolfocolavito@hotmail.it>
2025-01-13 15:17:31 +0100

    "cambio didascalia mappa"

--- Script/mapSetup.js
+++ Script/mapSetup.js
@@ -9,5 +9,5 @@ map.zoomControl.setPosition('bottomright');
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | icon-pack: <a href="https://www.flaticon.com/free-icons/uni" title="uni icons">Uni icons created by juicy_fish - Flaticon</a>',
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | <a href="https://www.flaticon.com/free-icons/uni" title="uni icons">Uni icons created by juicy_fish - Flaticon</a>',
 }).addTo(map);
