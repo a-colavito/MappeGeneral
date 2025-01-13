6d48dfbea7b9234bf637838943419241e2a1d014
a-colavito <adolfocolavito@hotmail.it>
2025-01-13 19:29:47 +0100

    aggiunta view mobile

--- Script/mobileView.js
+++ Script/mobileView.js
@@ -1,10 +1,8 @@
-
 document.addEventListener("DOMContentLoaded", function () {
     const mapMenu = document.getElementById("map-menu");
     const menuHeader = mapMenu.querySelector(".menu-header");
 
     menuHeader.addEventListener("click", function () {
-        // Aggiunge/rimuove la classe "expanded" per espandere/ridurre il menu
         mapMenu.classList.toggle("expanded");
     });
 });
