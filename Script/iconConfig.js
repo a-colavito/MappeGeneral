1e18234e78709a854f97b926b198347aadbd4665
a-colavito <adolfocolavito@hotmail.it>
2025-01-14 10:24:11 +0100

    aumento dimensione icone

--- Script/iconConfig.js
+++ Script/iconConfig.js
@@ -1,49 +1,49 @@
 export const iconConfig = {
     library: L.icon({
         iconUrl: 'Icone/library-icon.png',
-        iconSize: [32, 32],
+        iconSize: [36, 36],
         iconAnchor: [16, 32],
         popupAnchor: [0, -32],
     }),
     chapel: L.icon({
         iconUrl: 'Icone/chapel-icon.png',
-        iconSize: [32, 32],
+        iconSize: [36, 36],
         iconAnchor: [16, 32],
         popupAnchor: [0, -32],
     }),
     restaurant: L.icon({
         iconUrl: 'Icone/restaurant-icon.png',
-        iconSize: [32, 32],
+        iconSize: [36, 36],
         iconAnchor: [16, 32],
         popupAnchor: [0, -32],
     }),
     auditorium: L.icon({
         iconUrl: 'Icone/auditorium-icon.png',
-        iconSize: [32, 32],
+        iconSize: [36, 36],
         iconAnchor: [16, 32],
         popupAnchor: [0, -32],
     }),
     desk: L.icon({
         iconUrl: 'Icone/desk-icon.png',
-        iconSize: [32, 32],
+        iconSize: [36, 36],
         iconAnchor: [16, 32],
         popupAnchor: [0, -32],
     }),
     hospital: L.icon({
         iconUrl: 'Icone/hospital-icon.png',
-        iconSize: [32, 32],
+        iconSize: [36, 36],
         iconAnchor: [16, 32],
         popupAnchor: [0, -32],
     }),
     consueling: L.icon({
         iconUrl: 'Icone/consueling-icon.png',
-        iconSize: [32, 32],
+        iconSize: [36, 36],
         iconAnchor: [16, 32],
         popupAnchor: [0, -32],
     }),
     atm: L.icon({
         iconUrl: 'Icone/atm-icon.png',
-        iconSize: [32, 32],
+        iconSize: [36, 36],
         iconAnchor: [16, 32],
         popupAnchor: [0, -32],
     })
